549ff7a1b6a0fcf309a35bc4af423b2d685dd855
Natasha Buckham <natashaisobel@gmail.com>
2024-07-03 14:42:00 +0100

    Creates array of times to sort

--- busboard.js
+++ busboard.js
@@ -1,15 +1,21 @@
 fetch("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals")
     .then(response => response.json())
     .then(body => 
-        {for (let i = 0; i < 5; i++) {
+        {
+            let timesToSort = [];
 
-            let busNumber = body[i].lineId;
-            let dateTime = new Date(Date.parse(body[i].expectedArrival))
-            let arrivalTime = dateTime.toLocaleTimeString('en-GB');
-            let arrivalDate = dateTime.toLocaleDateString('en-GB');
+            for (let i = 0; i < 5; i++) {
+                let busNumber = body[i].lineId;
+                let dateTime = new Date(Date.parse(body[i].expectedArrival))
+                timesToSort.push({[body[i].lineId]: dateTime});
+            };
+            console.log(body);
+            console.log(timesToSort);
+            // let arrivalTime = dateTime.toLocaleTimeString('en-GB');
+            // let arrivalDate = dateTime.toLocaleDateString('en-GB');
 
-            console.log(`Bus number ${busNumber} will arrive at ${arrivalTime} on ${arrivalDate}.`);
-        }}
+            // console.log(`Bus number ${busNumber} will arrive at ${arrivalTime} on ${arrivalDate}.`);
+        }
     );
 
     
