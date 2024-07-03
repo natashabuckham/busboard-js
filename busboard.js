31a834d86208df4abbc7fd34dce7217f58327d3d
anum1608 <riaz.anum@outlook.com>
2024-07-03 14:26:37 +0100

    added await before fetch, created fetchData(), outputData(), printNext5Buses()

--- busboard.js
+++ busboard.js
@@ -1,3 +1,31 @@
-fetch("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals")
-    .then(response => response.json())
-    .then(body => console.log(body));
+//fetch("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals")
+//    .then(response => response.json())
+//    .then(body => console.log(body));
+
+
+async function fetchData() {
+    try {
+        const response = await fetch("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals");
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+async function outputData() {
+    const data = await fetchData();
+
+    if (!data) {
+        return;
+    }
+    console.log(data)
+}
+
+async function printNext5Buses() {
+
+}
+
+
+
+outputData() ;
