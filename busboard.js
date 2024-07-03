a8a0b0cd877d8c1539c87cf648adf4b844c51e4a
Natasha Buckham <natashaisobel@gmail.com>
2024-07-03 13:25:34 +0100

    Part 1: prints five unordered  buses that stop

--- busboard.js
+++ busboard.js
@@ -1,3 +1,15 @@
 fetch("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals")
     .then(response => response.json())
-    .then(body => console.log(body));
+    .then(body => 
+        {for (let i = 0; i < 5; i++) {
+
+            let busNumber = body[i].lineId;
+            let dateTime = new Date(Date.parse(body[i].expectedArrival))
+            let arrivalTime = dateTime.toLocaleTimeString('en-GB');
+            let arrivalDate = dateTime.toLocaleDateString('en-GB');
+
+            console.log(`Bus number ${busNumber} will arrive at ${arrivalTime} on ${arrivalDate}.`);
+        }}
+    );
+
+    
